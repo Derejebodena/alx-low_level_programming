6af88f98b3b9b8eb681995961d8220866ca798f8
Derejebodena <derejebodena2015@gmail.com>
2022-12-10 05:46:56 -0800

    T-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  *  main - prints the phrase with proper grammer,
  *  but the outcome is a piece of art,
@@ -9,5 +10,6 @@ int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art, \n");
 	return (0);
+
 }
 
