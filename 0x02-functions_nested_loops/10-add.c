738cd70e19a982396b6698c1c249a7b08811087e
Derejebodena <derejebodena2015@gmail.com>
2022-12-17 03:46:17 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - print add two numbers
+ * @i: print int i
+ * @k: print int k
+ * Return: results
+ */
+int add(int i, int k)
+{
+	results = i + k;
+
+	return (results);
+}
