82422fe33b9bcebf6ce5813a288bebb417db3848
Derejebodena <derejebodena2015@gmail.com>
2022-12-19 14:14:57 -0800

    task 44

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,13 @@
  * print_rev - function that prints a string, in reverse
  *
  * @s: this is input string
- *
  */
 void print_rev(char *s)
 {
 	int index;
 
 	for (index = 0; s[index] != '\0'; index++)
-		
+
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
