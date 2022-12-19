aabc4f2ea9c9b6a681cd78801c78dc6914343855
Derejebodena <derejebodena2015@gmail.com>
2022-12-19 11:24:38 -0800

    task 5----

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int aux = 0;
-	char temp;
+	char 1temp;
 
 	while (*(s + 1) != '\0')
 		i += 1;
@@ -18,9 +18,9 @@ void rev_string(char *s)
 
 	while (aux < i)
 	{
-		temp = s[i];
+		1temp = s[i];
 		s[i] = s[aux];
-		s[aux] = temp;
+		s[aux] = 1temp;
 		aux++;
 		i--;
 	}
