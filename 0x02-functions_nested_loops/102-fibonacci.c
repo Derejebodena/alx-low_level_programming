17b04823745672d268e15471f9fdf595f9e3b8f9
Derejebodena <derejebodena2015@gmail.com>
2022-12-23 04:33:41 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 0; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	return (0);
+}
