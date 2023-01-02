89bfa770ef8770ec45373a8a29e70cf6f8e50d38
Derejebodena <derejebodena2015@gmail.com>
2023-01-02 13:00:15 -0800

    task 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,12 +16,12 @@ char *_strstr(char *haystack, char *needle)
 		str1 = haystack;
 		str2 = needle;
 
-		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
-		{
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)		{
 			haystack++;
 			str2++;
 		}
-		if (*str2 != '\0')
+		if (*str2 == '\0')
+		{
 			return (str1);
 		haystack = str1 + 1;
 	}
